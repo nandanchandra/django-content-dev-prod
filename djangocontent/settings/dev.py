baeeecb7af567e75efd103e95a15859540b44fb9
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2022-09-04 21:03:38 +0530

    crfs-fix

--- djangocontent/settings/dev.py
+++ djangocontent/settings/dev.py
@@ -10,7 +10,14 @@ APPS_DIR =ROOT_DIR/"api"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", True)
 
-CSRF_TRUSTED_ORIGINS = ['http://.127.0.0.1']
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    ]
 
 # Application definition
 
