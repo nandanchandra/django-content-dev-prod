d9e4735064ba7107403f7ecd09ae0d3ea58f97cd
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2022-10-05 14:14:58 +0530

    isort-sorting

--- api/account/forms.py
+++ api/account/forms.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import get_user_model
 from django.contrib.auth import forms as admin_forms
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
--- api/account/models.py
+++ api/account/models.py
@@ -1,12 +1,11 @@
 import uuid
 
-from django.db import models
-from django.utils import timezone
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
+from django.db import models
+from django.utils import timezone
 
 
 class CustomUserManager(BaseUserManager):
