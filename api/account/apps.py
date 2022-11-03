3d8d0dcef339cb33d47bfa3d1526c147174767cc
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2022-11-03 20:08:16 +0530

    profile-signal-added

--- api/account/apps.py
+++ api/account/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AccountConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "api.account"
+
+    def ready(self):
+        from api.account import signals
